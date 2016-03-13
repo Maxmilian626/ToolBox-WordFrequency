da559bd3baef49ed50906798ad2ee23d17a10212
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-03-12 19:20:53 -0500

    If I did both this and using the Reddit API for my project, can I count that as a double?

--- frequency.py
+++ frequency.py
@@ -1,7 +1,9 @@
 """ Analyzes the word frequencies in a book downloaded from
 	Project Gutenberg """
-
 import string
+
+
+
 
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
@@ -9,7 +11,14 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	source = open(file_name)
+	txt = source.read()
+	for i in txt:
+		if i in string.punctuation:
+			txt.replace(i, "")
+	word_list = text.split()
+	return text.split()
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +30,22 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	pass 
+
+counter = dict()
+count_list[]
+
+word_list = get_word_list("pg32325.txt")
+for word in word_list:
+	if word not in counter:
+		counter[word] = 1
+	else: 
+		counter[word] += 1
+
+for word in word_list:
+	count_list.append(counter[word], word)
+
+count_list.sort(reverse=True)
+
+top100 = count_list[0:100]
+print top100 #the buzzwords delivered.  
